02293e1ba3eef6364d2f854f1f4a742d1cb70010
Jaime <Jaimedgp>
2018-07-10 14:20:45 +0200

    Continue playing

--- PlayNcourses.cpp
+++ PlayNcourses.cpp
@@ -1,12 +1,57 @@
+/**
+ * Play with Ncurses showing the Data obtained from USB 
+ *
+ * @author Jaimedgp
+ */
+
 #include <ncurses.h>
 
-int main () {
+int main (int argc, char *argv[]) {
 
-    initscr(); // start curses mode
-    printw("Hello World !!!"); // Print hello world
-    refresh(); // print it on to the real screen
-    getch(); // wait for user input
-    endwin(); //end curses mode
+    WINDOW *plotAllData, *plotSelectData;
+    
+    int ch;
 
+    noecho();
+    initscr();
+    cbreak();
+    keypad(stdscr, TRUE);
+
+    refresh();
+
+    plotAllData = newwin(LINES-1, 2*COLS/3, 1, COLS/3);
+    box(plotAllData, 0, 0);
+    wrefresh(plotAllData);
+
+    plotSelectData = newwin(LINES-1, COLS/3, 1, 0);
+    box(plotSelectData, 0, 0);
+    wrefresh(plotSelectData);
+
+
+    for (int i = 0; i < 100000; i++) {
+        printw("HOLA CHICOS");
+    
+
+        if ((ch = getch()) == 'A') {
+            break;
+        } else {            
+            switch (ch) {
+                case KEY_RIGHT:
+                    wprintw(plotAllData, "1 \n");
+                    wrefresh(plotAllData);
+                    break;
+                case KEY_LEFT:
+                    wprintw(plotSelectData, "2 \n");
+                    wrefresh(plotSelectData);
+                    break;
+                default:
+                    NULL;
+            }
+
+        }
+    }
+
+    endwin();
     return 0;
+
 }
