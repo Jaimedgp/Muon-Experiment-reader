ea5f0230a12f2092c1ec7833be5337c290e0e041
Jaimedgp <jaime.diez.gp@gmail.com>
2018-07-16 14:16:52 +0200

    The good example with the final appearence

--- main.cpp
+++ main.cpp
@@ -12,27 +12,35 @@ int main (int argc, char **argv) {
 
 	refresh();
 
-	plotAll1 = newwin((LINES-2)/2, 2*COLS/3, 2, 0);
+	plotAll1 = newwin(2*(LINES)/3, 3*COLS/4, 0, 0);
 	box(plotAll1, 0, 0);
 	wrefresh(plotAll1);
 
-	plotAll2 = newwin((LINES-2)/2, 2*COLS/3, 2+(LINES-2)/2, 0);
+	plotAll2 = newwin((LINES)/3, COLS, 2*(LINES)/3, 0);
 	box(plotAll2, 0, 0);
 	wrefresh(plotAll2);
 
-	plotSelect1 = newwin((LINES-2)/2, COLS/3, 2, 2*COLS/3);
+	plotSelect1 = newwin((LINES)/3, COLS/4, 0, 3*COLS/4);
 	box(plotSelect1, 0, 0);
-	wrefresh(plotSelect1);
 
-    mvwprintw(plotSelect1, 5, 5, "Elapsed Time: 200");
-    mvwprintw(plotSelect1, 10, 5, "Number of Muons: 100");
-    mvwprintw(plotSelect1, 15, 5, "Muonn Rate (per second): 3");
-    mvwprintw(plotSelect1, 20, 5, "Muon Decays: 4");
-    mvwprintw(plotSelect1, 25, 5, "Decay Rate (per minute): 1");
+    mvwprintw(plotSelect1, 2, 10, "Start");
+    mvwprintw(plotSelect1, 5, 10, "Pause");
+    mvwprintw(plotSelect1, 8, 10, "Fit");
+    mvwprintw(plotSelect1, 11, 10, "View Raw Data");
+    mvwprintw(plotSelect1, 14, 10, "Configure");
+    mvwprintw(plotSelect1, 17, 10, "Quit");
+
     wrefresh(plotSelect1);
 	
-	plotSelect2 = newwin((LINES-2)/2, COLS/3, 2+(LINES-2)/2, 2*COLS/3);
+	plotSelect2 = newwin((LINES)/3, COLS/4, (LINES)/3, 3*COLS/4);
 	box(plotSelect2, 0, 0);
+
+    mvwprintw(plotSelect2, 5, 5, "Elapsed Time: 200");
+    mvwprintw(plotSelect2, 7, 5, "Number of Muons: 100");
+    mvwprintw(plotSelect2, 9, 5, "Muonn Rate (per second): 3");
+    mvwprintw(plotSelect2, 11, 5, "Muon Decays: 4");
+    mvwprintw(plotSelect2, 13, 5, "Decay Rate (per minute): 1");
+
 	wrefresh(plotSelect2);
 
 	Histogram h(argc, argv, plotAll1);
